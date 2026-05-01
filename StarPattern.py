06070d40a8fce5cca5008206482f3bebf5ea3f3b
itsrajchauhan9044-rgb <itsrajchauhan9044@gmail.com>
2026-05-01 15:50:29 +0530

    Add files via upload

---
+++ StarPattern.py
@@ -0,0 +1,9 @@
+# for i in range(1, 5):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        print("*", end=" ")
+      print()
